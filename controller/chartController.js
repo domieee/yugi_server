d5666e774bd5f9e28c3b5215571cf394ae1ff59b
domieee <gartzdominik@gmail.com>
2023-06-08 10:42:22 +0200

    :rocket: add: in controller/charController.js: added tournament-breakdown route

--- controller/chartController.js
+++ controller/chartController.js
@@ -1,44 +1,54 @@
 export const getTournamentBreakdown = async (req, res, next) => {
     const tournamentId = req.body.id
+    try {
 
-    const pipeline = [
-        {
-            $match: {
-                _id: ObjectId(tournamentId)
+
+        const db = await connectDatabase()
+        const tournaments = db.collection('tournaments')
+
+        const pipeline = [
+            {
+                $match: {
+                    _id: ObjectId(tournamentId)
+                }
+            },
+            {
+                $unwind: "$player"
+            },
+            {
+                $group: {
+                    _id: "$player.deck",
+                    count: { $sum: 1 }
+                }
             }
-        },
-        {
-            $unwind: "$player"
-        },
-        {
-            $group: {
-                _id: "$player.deck",
-                count: { $sum: 1 }
-            }
-        }
-    ]
+        ]
 
-    const cursor = tournaments.aggregate(pipeline)
-    let percentageValue = 0
+        const cursor = tournaments.aggregate(pipeline)
+        let percentageValue = 0
 
-    await cursor.forEach(result => {
-        values.push(result._id)
-        counts.push(result.count)
-    })
+        await cursor.forEach(result => {
+            values.push(result._id)
+            counts.push(result.count)
+        })
 
-    counts.forEach(count => {
-        percentageValue += count
-    })
+        counts.forEach(count => {
+            percentageValue += count
+        })
 
-    counts.forEach(count => {
-        let percentage = (count * 100) / percentageValue;
-        percentage = percentage.toFixed(2)
-        percentages.push(percentage)
-    })
+        counts.forEach(count => {
+            let percentage = (count * 100) / percentageValue;
+            percentage = percentage.toFixed(2)
+            percentages.push(percentage)
+        })
 
-    data.push(values)
-    data.push(counts)
-    data.push(percentages)
+        data.push(values)
+        data.push(counts)
+        data.push(percentages)
 
-    res.status(200).json(data)
+        res.status(200).json(data)
+    } catch (error) {
+        console.log(error)
+    }
+
+
 }
