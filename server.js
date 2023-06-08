cef38b823b8cc3de5e1ee8b7ff8038009aed9897
domieee <gartzdominik@gmail.com>
2023-06-08 11:04:14 +0200

    :rocket: add: in controller/charController.js: added tournament-breakdown route

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express()
 
 const PORT = process.env.PORT || process.env.FALLBACK_PORT
 
-app.use(cors({ origin: process.env.FRONTEND_URL }))
+app.use(cors())
 app.use(express.json())
 
 app.get('/', async (req, res) => {
