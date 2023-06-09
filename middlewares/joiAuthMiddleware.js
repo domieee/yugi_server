037f18c585465045973898370acc25e2977548a4
domieee <gartzdominik@gmail.com>
2023-06-09 15:43:24 +0200

    :rocket: update: added /register route to vercel.json

--- middlewares/joiAuthMiddleware.js
+++ middlewares/joiAuthMiddleware.js
@@ -22,7 +22,7 @@ export async function validateRegisterInput(req, res, next) {
 
     try {
         const value = await schema.validateAsync({ username: req.body.username, email: req.body.mail, password: req.body.password, repeat_password: req.body.confirmPassword })
-        res.send(200).json({ msg: 'Registration successful' })
+        res.status(200).json({ msg: 'Registration successful' })
     } catch (err) {
         if (err.details[0].type === 'string.empty' && err.details[0].context.key === 'username') {
             res.status(400).json({ msg: 'Username is required', key: 'username' })
