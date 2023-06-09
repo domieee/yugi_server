0f7844207fce64a7441017f882bae105659fa4b5
domieee <gartzdominik@gmail.com>
2023-06-09 12:09:20 +0200

    :rocket: update: changed the '/' route from POST to GET

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || process.env.FALLBACK_PORT
 app.use(cors({ origin: '*' }))
 app.use(express.json())
 
-app.post('/', async (req, res) => {
+app.get('/', async (req, res) => {
 
     const db = await connectDatabase()
     const json = await db.collection('tournaments').find({}).toArray()
