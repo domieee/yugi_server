e0120b22f72526a2fda471b7950470b1096cc7f4
domieee <gartzdominik@gmail.com>
2023-06-09 17:16:22 +0200

    :rocket: update: added /register route to vercel.json

--- middlewares/joiAuthMiddleware.js
+++ middlewares/joiAuthMiddleware.js
@@ -26,18 +26,25 @@ export async function validateRegisterInput(req, res, next) {
     } catch (err) {
         if (err.details[0].type === 'string.empty' && err.details[0].context.key === 'username') {
             res.status(400).json({ msg: 'Username is required', key: 'username' })
+            return
         } else if (err.details[0].type === 'string.min' || err.details[0].type === 'string.max' && err.details[0].context.key === 'username') {
             res.status(400).json({ msg: 'Username should contain min. 2 and max. 30 characters', key: 'username' })
+            return
         } else if (err.details[0].type === 'string.empty' && err.details[0].context.key === 'email') {
             res.status(400).json({ msg: 'Email is required', key: 'email' })
+            return
         } else if (err.details[0].type === 'string.email' && err.details[0].context.key === 'email') {
             res.status(400).json({ msg: 'Invalid email format', key: 'email' })
+            return
         } else if (err.details[0].type === 'string.empty' && err.details[0].context.key === 'password') {
             res.status(400).json({ msg: 'Password is required', key: 'password' })
+            return
         } else if (err.details[0].type === 'string.pattern.base' && err.details[0].context.key === 'password') {
             res.status(400).json({ msg: 'Password do not match the required pattern. /^[a-zA-Z0-9]{3,30}$/', key: 'password' })
+            return
         } else {
             res.status(400).json({ msg: 'Something went wrong while logging in. Please try it again.', key: 'password' })
+            return
         }
     }
 }
