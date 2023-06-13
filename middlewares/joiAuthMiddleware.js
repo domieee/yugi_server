57df88e4be995c0c1b524f7d57ade3d35fafa68f
domieee <gartzdominik@gmail.com>
2023-06-13 22:17:19 +0200

    push

--- middlewares/joiAuthMiddleware.js
+++ middlewares/joiAuthMiddleware.js
@@ -18,13 +18,15 @@ export async function validateRegisterInput(req, res, next) {
         password: Joi.string()
             .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
 
-        repeat_password: Joi.ref('password'),
+        repeat_password: Joi.ref('password')
+            .required(),
     })
 
     try {
         const value = await schema.validateAsync({ username: req.body.username, email: req.body.email, password: req.body.password, repeat_password: req.body.confirmPassword })
         next()
     } catch (err) {
+        console.log(err.details)
         if (err.details[0].type === 'string.empty' && err.details[0].context.key === 'username') {
             res.status(400).json({ msg: 'Username is required', key: 'username' })
             return
@@ -41,7 +43,10 @@ export async function validateRegisterInput(req, res, next) {
             res.status(400).json({ msg: 'Password is required', key: 'password' })
             return
         } else if (err.details[0].type === 'string.pattern.base' && err.details[0].context.key === 'password') {
-            res.status(400).json({ msg: 'Password do not match the required pattern. /^[a-zA-Z0-9]{3,30}$/', key: 'password' })
+            res.status(400).json({ msg: 'Password do not match the required pattern. /^[a-zA-Z0-9]{8,30}$/', key: 'password' })
+            return
+        } else if (err.details[0].type === 'any.only' && err.details[0].context.key === 'repeat_password') {
+            res.status(400).json({ msg: 'Passwords doesn`t match', key: 'password' })
             return
         } else {
             res.status(400).json({ msg: 'Something went wrong while logging in. Please try it again.', key: 'password' })
