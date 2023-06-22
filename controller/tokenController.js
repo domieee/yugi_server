58da8dcda2efb758f04bc7bb5f1bf03f00837500
domieee <gartzdominik@gmail.com>
2023-06-22 18:06:39 +0200

    :heave_plus_sign: in tokenController: if the session is expired when trying to receive the userinformatiion, we send back 401 and handle the error in the front end

--- controller/tokenController.js
+++ controller/tokenController.js
@@ -14,18 +14,30 @@ export const receiveUserInformations = (req, res, next) => {
         res.json(decodedToken)
         console.log(decodedToken)
     } catch (err) {
-        res.json({ msg: 'Your Session is expired. Please login again.' })
+        if (err.name === 'TokenExpiredError') {
+            res.status(401).json({ msg: 'Your Session is expired. Please login again.' })
+        } else {
+        }
     }
 }
 
 export const validateModeratorAction = async (req, res, next) => {
 
-    console.log('UserID', req.body.userId)
-    const decodedToken = await jwt.verify(req.body.userId, process.env.JWT_SECRET);
-    console.log('decodedToken', decodedToken)
-    if (decodedToken.role === 'moderator' || decodedToken.role === 'administrator') {
-        next()
-    } else {
-        res.status(401).json({ msg: 'You are not allowed to perform this action.' })
+    try {
+        console.log('UserID', req.body.userId)
+        const decodedToken = jwt.verify(req.body.userId, process.env.JWT_SECRET);
+        console.log('decodedToken', decodedToken)
+        if (decodedToken.role === 'moderator' || decodedToken.role === 'administrator') {
+            next()
+        }
+    } catch (err) {
+        console.log(err)
+        if (err.name === 'TokenExpiredError') {
+            res.json({ msg: 'Your Session is expired. Please login again.' })
+        } else {
+
+        }
     }
+
+
 }
